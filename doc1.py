11d0986cbd39c46d21fbecf88fd920d15d625b6a
gusanare1 <gang_emelec@yahoo.com>
2018-11-23 15:06:04 -0500

    Cambios menores

--- doc1.py
+++ doc1.py
@@ -81,32 +81,46 @@ def crear_docx(numero=1):
 	from lxml import etree
 
 	i=2
-	tam=0.8
+	tam=1.2
+	numero_preguntas = len(preguntas)
+	print("NUM_PREG"+str(numero_preguntas))
 	for pregunta in preguntas:
 		p = document.add_paragraph("", style='List Number')
 		p.add_run(pregunta.literal).bold=True
 		if pregunta.ecuacion:
-			
-			preg=sim.ecuacion_to_latex(pregunta.ecuacion)
-			
-			#print("Preg::"+preg)
-			#eq=ecuacion_to_jpg(pregunta.ecuacion, "eqx")
-			#document.add_picture("eqx.png",width=Inches(0.8))
-			
-			if isinstance(preg, (np.ndarray, np.generic) ):
-				sim.crear_foto(preg,"preg.png",esMatriz=True)
-				tam=1.4
-			else:#no es matriz... ecuacion imagino...
-				sim.crear_foto(preg,"preg.png")
-				if len(preg)>60:
-					tam=1.5
-				else:
-					tam=0.8
-			p=document.add_paragraph()
-			p.add_run().add_picture("preg.png",width=Inches(tam))
-			#print("Pr:"+preg+repr(tam))
-			last_paragraph = document.paragraphs[-1] 
-			last_paragraph.alignment = WD_ALIGN_PARAGRAPH.CENTER
+			if ".jpg" in pregunta.ecuacion or ".png" in pregunta.ecuacion:
+				p=document.add_paragraph()
+				p.add_run().add_picture(pregunta.ecuacion,width=Inches(1.7))
+				
+			else:
+				preg=sim.ecuacion_to_latex(pregunta.ecuacion)
+				
+				if isinstance(preg, (np.ndarray, np.generic) ):
+					sim.crear_foto(preg,"preg.png",esMatriz=True)
+					tam=1.4
+				else:#no es matriz... ecuacion imagino...
+					sim.crear_foto(preg,"preg.png")
+					if len(preg)>70:
+						tam=3
+						#print("1P")
+						#print(preg)
+					elif len(preg)>60:
+						#print("2")
+						#print(preg)
+						tam=2.2
+					elif len(preg)>45:
+						tam=1.9
+					elif len(preg)>30:
+						tam=1.6
+					elif len(preg)>30:
+						tam=1.3
+					else:
+						tam=0.9
+				p=document.add_paragraph()
+				p.add_run().add_picture("preg.png",width=Inches(tam))
+				#print("Pr:"+preg+repr(tam))
+				last_paragraph = document.paragraphs[-1] 
+				last_paragraph.alignment = WD_ALIGN_PARAGRAPH.CENTER
 				
 			
 		sol = 97
@@ -115,24 +129,59 @@ def crear_docx(numero=1):
 			
 				res=sim.ecuacion_to_latex(resp)
 				p=document.add_paragraph(chr(sol)+") ")
-				
+				if len(res)>100:
+						tam=2.7
+						#print("r1")
+						#print(res)
+				elif len(res)>90:
+						tam=2.3
+						#print("r1")
+						#print(res)
+				elif len(res)>75:
+						#print("r2")
+						tam=2.0
+						#print(res)
+				elif len(res)>60:
+						tam=1.7
+						#print("r3")
+						#print(res)
+				elif len(res)>45:
+						tam=1.4
+						#print("r4")
+						#print(res)
+				elif len(res)>30:
+						tam=1.1
+						#print("r5")
+						#print(res)
+				elif len(res)>15:
+						tam=0.8
+						#print("r6")
+						#print(res)
+				else :
+						tam=0.55
+						#print("r7")
+						#print(res)
 				if isinstance(res, (np.ndarray, np.generic) ):
 					sim.crear_foto(res,"respM.png",tam=120,esMatriz=True)
 					#print("Matrix")
-					p.add_run().add_picture("respM.png",width=Inches(1.3))
+					p.add_run().add_picture("respM.png",width=Inches(tam))
 				else:
-					sim.crear_foto(res,"resp.png",tam=180)
-					p.add_run().add_picture("resp.png",width=Inches(0.4))
+					sim.crear_foto(res,"resp.png",tam=130)
+					p.add_run().add_picture("resp.png",width=Inches(tam))
 				
 				sol=sol+1
-		if i%2==0:
+		print("Pregunta: "+str(i))
+		if i%2==0 and i<numero_preguntas-1:
 			document.add_page_break()
+			print("NEXT PAGE")
 		else:
-			for j in range(0,4):
-				p = document.add_paragraph()
-				run = p.add_run()
-				run.add_break()
-			run.add_break()
+			if not i==numero_preguntas+1:
+				for j in range(0,4):
+					print("*Enter*")
+					p = document.add_paragraph()
+					run = p.add_run()
+					run.add_break()
+				#run.add_break()
 		i=i+1	
 
 		
